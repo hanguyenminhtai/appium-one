e17b733b5b64baaff0db6f6d6a8d8e3897da2a62
ha tai <hanguyenminhtai@gmail.com>
2023-08-29 09:45:53 +0700

    lab66

--- src/lab_6/Lab61.java
+++ src/lab_6/Lab61.java
@@ -2,10 +2,11 @@ package lab_6;
 
 public class Lab61 {
     public static void main(String[] args) {
-
         String myString = "2hrs and 5minutes";
-        String hString = myString.substring(0, 1);
-        String mString = myString.substring(9, 10);
+
+        String[] strSpilit = myString.split(" and ");
+        String hString = strSpilit[0].substring(0, 1);
+        String mString = strSpilit[1].substring(0, 1);
         int hString1 = Integer.valueOf(hString);
         int mString1 = Integer.valueOf(mString);
         System.out.println("Total minutes: " + (hString1 * 60 + mString1));
--- src/lab_6/Lab64.java
+++ src/lab_6/Lab64.java
@@ -2,28 +2,13 @@ package lab_6;
 
 public class Lab64 {
     public static void main(String[] args) {
+        String url = "https://google.com";
+        String[] urlSplit = url.split("://");
+        String protocal = urlSplit[0];
+        String domainName = urlSplit[1];
 
-        String url = "https://google.comm";
-        int checkUrl = url.indexOf("https");
-
-        if (checkUrl != 0) {
-            System.out.println("This is http");
-        } else {
-            System.out.println("This is https");
-        }
-
-        String subUrl= url.substring(8);
-        if (subUrl.equals("google.com")){
-            System.out.println("It's domain name");
-        }else {
-            System.out.println("Warning!");
-        }
-
-        String subUrl1= url.substring(14);
-        if(subUrl1.equals(".com")){
-            System.out.println("It's .com");
-        }else{
-            System.out.println("It's .net or st");
-        }
+        System.out.println(protocal.equals("https"));
+        System.out.println(domainName);
+        System.out.println(".com or .net?--> its:" + domainName.substring(domainName.length() - 4, domainName.length()));
     }
 }
